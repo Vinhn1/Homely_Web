a0d993f4648c2b8d4962a06c098380e529fda88f
Vinhn1 <vinhnguyen04275@gmail.com>
2026-03-29 07:37:50 +0000

    Xây dựng authMiddlewares Xác thực người dùng

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+// Cổng bảo vệ xác thực đúng mới cho vào routes 
+export const verifyToken = async (req, res, next) => {
+ try {
+    // Lấy token từ header
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+    if(!token){
+        return res.status(401).json({message: "Không tìm thấy access token"});
+    }
+
+
+    // Xác nhận token hợp lệ
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // tìm user
+    const user = await User.findById(decoded.userId);
+    if(!user){
+        return res.status(404).json({message: "Người dùng không tồn tại."}); 
+    }
+
+    // Gắn user vào req 
+    req.user = user;
+    next();
+
+
+ }catch(error){
+    console.error("Lỗi xác minh jwt trong authMiddleware.");
+    return res.status(403).json({message: "Token không hợp lệ hoặc đã hết hạn"});
+ }
+}
