0474c47bc83d70b8ffdc1591495a99db551d702c
Vinhn1 <vinhnguyen04275@gmail.com>
2026-03-29 08:37:57 +0000

    triển khai authService để quản lý đăng ký, đăng nhập và lấy dữ liệu người dùng

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,26 @@
+import axiosInstance from "@/api/axios";
+// "đầu não" điều phối toàn bộ các yêu cầu liên quan đến xác thực.
+
+// ========================================================================
+// Nhận dữ liệu từ Form đăng ký và gửi lên Server
+export const signUp = async (userData) => {
+    // Gửi một yêu cầu POST mang theo dữ liệu người dùng (userData - bao gồm email, password, name...) lên ngõ /auth/signUp.
+    const response = await axiosInstance.post('/auth/signUp', userData);
+    // Thông báo "Đăng ký thành công" hoặc thông tin User mới từ Server gửi về.
+    return response.data;
+}
+
+// ========================================================================
+// Gửi yêu cầu POST chứa username và password lên ngõ /auth/signIn.
+export const signIn = async (credentials) => {
+    const response = await axiosInstance.post('/auth/signIn', credentials);
+    return response.data;
+}
+
+// ========================================================================
+// Hàm lấy thông tin cá nhân 
+export const getMe = async () => {
+    // Gửi một yêu cầu lên ngõ /auth/me.
+    const response = await axiosInstance.get('/auth/me');
+    return response.data;
+}
